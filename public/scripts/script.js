68156054ae67fc2f3b030ea6b0d1a3eb68638ad5
Pip <pip.harsveld@hva.nl>
2023-06-01 22:10:37 +0200

    :construction: Begin working on grid functionality

---
+++ public/scripts/script.js
@@ -0,0 +1,9 @@
+import Masonry from 'masonry-layout';
+
+//Grid layout
+window.onload = () => {
+    const grid = document.querySelector('.grid');
+    const masonry = new Masonry(grid);
+}
+
+console.log("Testje");

243063b057cfbeb0dd4fb979784e6856b6b34551
Inevdhoven <inevdhoven@gmail.com>
2023-06-07 21:43:38 +0200

    :sparkles: Added JavaScript for form validation

---
+++ public/js/script.js
@@ -0,0 +1,53 @@
+console.log('Hello from script.js');
+const wishForm = document.querySelector('.wish-form form');
+const wishTitle = document.querySelector('.wish-form form input#title');
+const wishDescription = document.querySelector('.wish-form form textarea#description');
+const wishError = document.querySelector('.wish-form form span.error');
+
+let titleErrorShown = false;
+let descriptionErrorShown = false;
+
+wishForm.setAttribute('novalidate', true);
+
+wishForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('Hello from wishForm');
+
+    if (wishTitle.value === '' && wishDescription.value === '') {
+        wishTitle.focus();
+        if (!titleErrorShown) {
+            wishTitle.insertAdjacentHTML('afterend', '<span class="error">Voeg alsjeblieft een titel voor je wens toe.</span>');
+            titleErrorShown = true;
+        }
+        if (!descriptionErrorShown) {
+            wishDescription.insertAdjacentHTML('afterend', '<span class="error">Voeg alsjeblieft een uitleg over je wens toe.</span>');
+            descriptionErrorShown = true;
+        }
+    } else if (wishTitle.value === '') {
+        if (descriptionErrorShown) {
+            wishDescription.nextElementSibling.remove();
+            descriptionErrorShown = false;
+        }
+        console.log('Please enter a title');
+        wishTitle.focus();
+        if (!titleErrorShown) {
+            wishTitle.insertAdjacentHTML('afterend', '<span class="error">Voeg alsjeblieft een titel voor je wens toe.</span>');
+            titleErrorShown = true;
+        }
+    } else if (wishDescription.value === '') {
+        if (titleErrorShown) {
+            wishTitle.nextElementSibling.remove();
+            titleErrorShown = false;
+        }
+        console.log('Please enter a description');
+        wishDescription.focus();
+        if (!descriptionErrorShown) {
+            wishDescription.insertAdjacentHTML('afterend', '<span class="error">Voeg alsjeblieft een uitleg over je wens toe.</span>');
+            descriptionErrorShown = true;
+        }
+    } else {
+        console.log('Formulier word verstuurd');
+        console.log(wishTitle.value);
+        console.log(wishDescription.value);
+    }
+});

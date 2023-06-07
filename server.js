8586bd06e94a5cbcbffeca0bb86d729c5439d51d
Inevdhoven <inevdhoven@gmail.com>
2023-06-07 21:45:23 +0200

    :card_file_box: Added Supabase

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ import dotenv from 'dotenv';
 import http from 'http';
 import handlebars from 'express-handlebars';
 import { router } from './router/routes.js';
+import bodyParser from 'body-parser';
+import { createClient } from '@supabase/supabase-js';
 
+const supabase = createClient(
+    'https://yyufywjwwwmgfjmenluv.supabase.co/rest/v1/',
+    `${process.env.API_KEY}`
+);
 const app = express();
 const port = process.env.PORT || 4400;
 const __dirname = path.resolve();
@@ -19,6 +25,7 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + '/static')); // Hier zit bijvoorbeeld css in
 app.use('/', express.static(__dirname + '/'));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.engine('hbs', handlebars.engine({
@@ -26,7 +33,14 @@ app.engine('hbs', handlebars.engine({
     extname: 'hbs',
     defaultLayout: 'index',
     partialsDir: __dirname + '/views/partials',
-}))
+}));
+
+app.get('/thema', async (req, res) => {
+    const { data, error } = await supabase
+        .from('thema')
+        .select()
+    res.send(data);
+});
 
 app.use('/', router);
 

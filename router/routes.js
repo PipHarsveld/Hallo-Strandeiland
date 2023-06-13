2b4f312a8ce0cafb61d4d0e4d7fca0095bd726d5
Inevdhoven <inevdhoven@gmail.com>
2023-06-13 14:35:47 +0200

    :white_check_mark: Added a new route

--- router/routes.js
+++ router/routes.js
@@ -6,7 +6,11 @@ dotenv.config();
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('form', { layout: 'index', title: 'Home' });
+    res.render('main', { layout: 'index', title: 'Home' });
+});
+
+router.get('/wens-toevoegen', (req, res) => {
+    res.render('form', { layout: 'index', title: 'Wens toevoegen' });
 });
 
 export { router }

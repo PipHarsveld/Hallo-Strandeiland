cac3f5f399cb8ae07c1a4a49cbf77830d8e031c0
Inevdhoven <inevdhoven@gmail.com>
2023-06-14 11:12:50 +0200

    :white_check_mark: Added some new routes

--- router/routes.js
+++ router/routes.js
@@ -1,51 +1,137 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import fetch from 'node-fetch';
 import bodyParser from 'body-parser';
 import { createClient } from '@supabase/supabase-js';
+import fetch from 'node-fetch';
 import { validationResult, body } from 'express-validator';
 
 dotenv.config();
 
-const router = express.Router();
 const supabase = createClient(
-    'https://yyufywjwwwmgfjmenluv.supabase.co/rest/v1/',
+    'https://yyufywjwwwmgfjmenluv.supabase.co/',
     `${process.env.API_KEY}`
 );
+
+const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('main', { layout: 'index', title: 'Home' });
 });
 
 router.get('/wens-toevoegen', async (req, res) => {
-    res.render('form', { layout: 'index', title: 'Wens toevoegen' });
+    try {
+        const { data, error } = await supabase
+            .from('thema')
+            .select();
+
+        if (error) {
+            throw new Error(`Error fetching data: ${error.message}`);
+        }
+
+        const themaLabels = data.map(thema => thema.label);
+        console.log(themaLabels);
+
+        // res.render('main', { layout: 'index', title: 'Thema', thema: themaLabels });
+        res.render('form', { layout: 'index', title: 'Wens toevoegen', thema: themaLabels });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
-router.post('/wens-toevoegen', [
-    body('title').notEmpty().withMessage('Voeg alsjeblieft een titel voor je wens toe.'),
-    body('description').notEmpty().withMessage('Voeg alsjeblieft een uitleg over je wens toe.'),
-], (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        const errorMessages = errors.array().map(error => error.msg);
+router.get('/thema', async (req, res) => {
+    try {
+        const { data, error } = await supabase
+            .from('thema')
+            .select();
 
-        return res.render('form', {
-            layout: 'index',
-            title: 'Wens toevoegen',
-            errors: errorMessages,
-            formData: req.body
-        });
+        if (error) {
+            throw new Error(`Error fetching data: ${error.message}`);
+        }
+
+        const themaLabels = data.map(thema => thema.label);
+        console.log(themaLabels);
+
+        res.render('main', { layout: 'index', title: 'Thema', thema: themaLabels });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/themas', async (req, res) => {
+    const { data, error } = await supabase
+        .from('thema')
+        .select();
+
+    if (error) {
+        // Handle the error appropriately (e.g., send an error response)
+        console.log(error);
     }
 
-    // Verwerk het formulier als er geen validatiefouten zijn
-    // ...
-
-    return res.render('success', {
-        layout: 'index',
-        title: 'Wens toevoegen',
-        message: 'Formulier succesvol verstuurd.'
-    });
+    if (data === null) {
+        // Handle the case when data is null (e.g., send an empty array [])
+        res.send(['empty array']);
+    } else {
+        res.send(data);
+    }
 });
+
+// router.post('/wens-toevoegen', [
+//     body('title').notEmpty().withMessage('Voeg alsjeblieft een titel voor je wens toe.'),
+//     body('description').notEmpty().withMessage('Voeg alsjeblieft een uitleg over je wens toe.'),
+// ], (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         const errorMessages = errors.array().map(error => error.msg);
+
+//         return res.render('form', {
+//             layout: 'index',
+//             title: 'Wens toevoegen',
+//             errors: errorMessages,
+//             formData: req.body
+//         });
+//     }
+
+//     // Verwerk het formulier als er geen validatiefouten zijn
+//     // ...
+
+//     return res.render('success', {
+//         layout: 'index',
+//         title: 'Wens toevoegen',
+//         message: 'Formulier succesvol verstuurd.'
+//     });
+// }); 
+
+// app.get('/thema', async (req, res) => {
+//     const { data, error } = await supabase
+//         .from('thema')
+//         .select();
+//     console.log(data);
+//     res.send(data);
+// });
+
+// function isValidImageUrl(url) {
+//     const imageRegex = /^https?:\/\/.*\.(png|jpg|jpeg|gif)(\?.*)?$/i;
+//     return imageRegex.test(url);
+// }
+
+// app.post('/upload', (req, res) => {
+//     const imageLink = req.body.imageLink;
+//     console.log(imageLink);
+
+//     if (!isValidImageUrl(imageLink)) {
+//         return res.status(400).send('Invalid image URL');
+//     }
+
+//     res.redirect('/image?link=' + imageLink);
+// });
+
+// app.get('/image', (req, res) => {
+//     const imageLink = req.query.link;
+//     console.log(imageLink + ' in /image');
+//     res.render('image', { imageLink });
+// });
 
 
 

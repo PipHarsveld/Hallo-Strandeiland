31cbb4f763fa68e1ce95d0fcda287743862ee811
Inevdhoven <inevdhoven@gmail.com>
2023-06-20 14:45:21 +0200

    :card_file_box: Wish with theme added to database

--- router/routes.js
+++ router/routes.js
@@ -39,119 +39,36 @@ router.get('/wens-toevoegen', async (req, res) => {
     }
 });
 
-// router.get('/theme', async (req, res) => {
-//     try {
-//         const { data, error } = await supabase
-//             .from('theme')
-//             .select();
-
-//         if (error) {
-//             throw new Error(`Error fetching data: ${error.message}`);
-//         }
-
-//         const themaLabels = data.map(thema => thema.label);
-//         console.log(themaLabels);
-
-//         res.render('main', { layout: 'index', title: 'Thema', thema: themaLabels });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-
-// router.get('/themas', async (req, res) => {
-//     const { data, error } = await supabase
-//         .from('thema')
-//         .select();
-
-//     if (error) {
-//         // Handle the error appropriately (e.g., send an error response)
-//         console.log(error);
-//     }
-
-//     if (data === null) {
-//         // Handle the case when data is null (e.g., send an empty array [])
-//         res.send(['empty array']);
-//     } else {
-//         res.send(data);
-//     }
-// });
-
 router.post('/wens', async (req, res) => {
-    // console.log(req.body.description)
     try {
-        const { error } = await supabase
+        const { data, error } = await supabase
             .from('suggestion')
-            .insert({ title: req.body.title, description: req.body.description, theme: 1, image: req.body.imageLink });
-        if (error) {
-            throw error;
+            .insert([{ title: req.body.title, description: req.body.description, image: req.body.imageLink }])
+            .select(); // De wens wordt toegevoegd aan de suggestion tabel
+
+        const insertId = data[0].id ?? null; // Er wordt een array teruggegeven, maar we willen alleen het id van de eerste entry hebben (die we net hebben toegevoegd) met dank aan de hulp van Maijla
+
+        if (error || !insertId) {
+            throw error;  // Als er een error is, of als er geen insertId is wordt er een error gegooid
         }
-        res.status(200).json({ message: 'Wens succesvol toegevoegd' });
+
+        const { error: themeError } = await supabase
+            .from('suggestion_theme')
+            .insert([{
+                suggestionId: insertId,
+                themaId: req.body.theme
+            }]); // De thema's worden toegevoegd aan de suggestion_theme tabel
+
+        if (themeError) {
+            throw themeError; // Als er een error is wordt er een error gegooid
+        }
+
+        res.render('main', { layout: 'index', message: 'Wens succesvol toegevoegd' });
     } catch (error) {
         res.status(500).json({ error: 'Er is een fout opgetreden bij het toevoegen van de wens' });
-        return; // Stop de uitvoering hier om dubbele reacties te voorkomen
+        console.log(error);
+        return;
     }
-    // Alleen omleiden als er geen fout is opgetreden
-    res.redirect('/wens');
 });
-
-
-// router.post('/wens-toevoegen', [
-//     body('title').notEmpty().withMessage('Voeg alsjeblieft een titel voor je wens toe.'),
-//     body('description').notEmpty().withMessage('Voeg alsjeblieft een uitleg over je wens toe.'),
-// ], (req, res) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//         const errorMessages = errors.array().map(error => error.msg);
-
-//         return res.render('form', {
-//             layout: 'index',
-//             title: 'Wens toevoegen',
-//             errors: errorMessages,
-//             formData: req.body
-//         });
-//     }
-
-//     // Verwerk het formulier als er geen validatiefouten zijn
-//     // ...
-
-//     return res.render('success', {
-//         layout: 'index',
-//         title: 'Wens toevoegen',
-//         message: 'Formulier succesvol verstuurd.'
-//     });
-// }); 
-
-// app.get('/thema', async (req, res) => {
-//     const { data, error } = await supabase
-//         .from('thema')
-//         .select();
-//     console.log(data);
-//     res.send(data);
-// });
-
-// function isValidImageUrl(url) {
-//     const imageRegex = /^https?:\/\/.*\.(png|jpg|jpeg|gif)(\?.*)?$/i;
-//     return imageRegex.test(url);
-// }
-
-// app.post('/upload', (req, res) => {
-//     const imageLink = req.body.imageLink;
-//     console.log(imageLink);
-
-//     if (!isValidImageUrl(imageLink)) {
-//         return res.status(400).send('Invalid image URL');
-//     }
-
-//     res.redirect('/image?link=' + imageLink);
-// });
-
-// app.get('/image', (req, res) => {
-//     const imageLink = req.query.link;
-//     console.log(imageLink + ' in /image');
-//     res.render('image', { imageLink });
-// });
-
-
 
 export { router }

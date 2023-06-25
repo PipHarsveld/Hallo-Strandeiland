30cb61ff989a6ad1e51e78031ce3828c237a41ce
Inevdhoven <inevdhoven@gmail.com>
2023-06-25 17:04:37 +0200

    :sparkles: Click button to add helper

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -80,6 +80,18 @@ if (filterButton) {
     });
 }
 
+// ADD HELPER
+const helperButton = document.querySelector('.wish section article button:nth-of-type(2)');
+const helper = document.querySelector('.wish aside section:nth-of-type(2) ul li:first-of-type');
+
+console.log(helperButton);
+
+if (helperButton) {
+    helperButton.addEventListener('click', () => {
+        helper.setAttribute('class', 'show');
+    });
+}
+
 // FORM VALIDATION
 const wishForm = document.querySelector('#wish-form');
 const wishTitle = document.querySelector('.wish-form form input#title');
@@ -91,107 +103,109 @@ let descriptionErrorShown = false;
 let imageLinkErrorShown = false;
 let themeErrorShown = false;
 
-wishForm.setAttribute('novalidate', true);
+if (wishForm) {
+    wishForm.setAttribute('novalidate', true);
 
-wishForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+    wishForm.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    const themeContainer = document.querySelector('.themes-container');
-    const themeCheckboxes = document.getElementsByName('theme');
-    const wishTitleValue = wishTitle.value.trim();
-    const wishDescriptionValue = wishDescription.value.trim();
-    const wishImageLinkValue = wishImageLink.value.trim();
-    let checked = false;
+        const themeContainer = document.querySelector('.themes-container');
+        const themeCheckboxes = document.getElementsByName('theme');
+        const wishTitleValue = wishTitle.value.trim();
+        const wishDescriptionValue = wishDescription.value.trim();
+        const wishImageLinkValue = wishImageLink.value.trim();
+        let checked = false;
 
-    themeCheckboxes.forEach(function (theme) {
-        if (theme.checked) {
-            checked = true;
+        themeCheckboxes.forEach(function (theme) {
+            if (theme.checked) {
+                checked = true;
+            }
+        });
+
+        // Reset error messages
+        removeErrorMessage(wishTitle);
+        removeErrorMessage(wishDescription);
+        removeErrorMessage(wishImageLink);
+        removeErrorMessage(themeContainer);
+
+        if (wishTitleValue.length < 3) {
+            displayError('Voeg alsjeblieft een titel voor je wens toe van minimaal 3 karakters.', wishTitle);
+        }
+
+        if (wishDescriptionValue.length < 10) {
+            displayError('Voeg alsjeblieft een uitleg over je wens toe van minimaal 10 karakters.', wishDescription);
+        }
+
+        if (wishImageLinkValue.length === 0) {
+            displayError('Voeg alsjeblieft een link naar een afbeelding toe.', wishImageLink);
+        } else if (!validateLink(wishImageLinkValue)) {
+            displayError('De opgegeven link naar een afbeelding is ongeldig.', wishImageLink);
+        }
+
+        if (!checked) {
+            displayError("Kies alsjeblieft een of meerdere thema's die passen bij de wens.", themeContainer);
+        }
+
+        if (wishTitleValue.length >= 3 && wishDescriptionValue.length >= 10 && checked && validateLink(wishImageLinkValue)) {
+            console.log('Formulier wordt verstuurd');
+            console.log(wishTitleValue);
+            console.log(wishDescriptionValue);
+            console.log(wishImageLinkValue);
+            wishForm.submit();
         }
     });
 
-    // Reset error messages
-    removeErrorMessage(wishTitle);
-    removeErrorMessage(wishDescription);
-    removeErrorMessage(wishImageLink);
-    removeErrorMessage(themeContainer);
-
-    if (wishTitleValue.length < 3) {
-        displayError('Voeg alsjeblieft een titel voor je wens toe van minimaal 3 karakters.', wishTitle);
-    }
-
-    if (wishDescriptionValue.length < 10) {
-        displayError('Voeg alsjeblieft een uitleg over je wens toe van minimaal 10 karakters.', wishDescription);
-    }
-
-    if (wishImageLinkValue.length === 0) {
-        displayError('Voeg alsjeblieft een link naar een afbeelding toe.', wishImageLink);
-    } else if (!validateLink(wishImageLinkValue)) {
-        displayError('De opgegeven link naar een afbeelding is ongeldig.', wishImageLink);
-    }
-
-    if (!checked) {
-        displayError("Kies alsjeblieft een of meerdere thema's die passen bij de wens.", themeContainer);
-    }
-
-    if (wishTitleValue.length >= 3 && wishDescriptionValue.length >= 10 && checked && validateLink(wishImageLinkValue)) {
-        console.log('Formulier wordt verstuurd');
-        console.log(wishTitleValue);
-        console.log(wishDescriptionValue);
-        console.log(wishImageLinkValue);
-        wishForm.submit();
-    }
-});
-
-function displayError(errorMessage, inputElement) {
-    if (!inputElement.classList.contains('error')) {
-        inputElement.insertAdjacentHTML('afterend', `<span class="error">${errorMessage}</span>`);
-        inputElement.classList.add('error');
-        document.querySelector('input:invalid, textarea:invalid, .error input').focus();
-    }
-}
-
-function removeErrorMessage(inputElement) {
-    if (inputElement.classList.contains('error')) {
-        const errorElement = inputElement.nextElementSibling;
-        errorElement.parentNode.removeChild(errorElement);
-        inputElement.classList.remove('error');
-    }
-}
-
-function validateLink(link) {
-    try {
-        const url = new URL(link);
-        return url.protocol === 'http:' || url.protocol === 'https:';
-    } catch (error) {
-        return false;
-    }
-}
-
-// DIALOG
-const dialogBtn = document.querySelector('#dialog-btn');
-const dialog = document.querySelector('#dialog');
-const dialogCloseBtn = document.querySelector('#dialog-close');
-
-dialogBtn.addEventListener('click', () => {
-    dialog.showModal();
-});
-
-dialogCloseBtn.addEventListener('click', () => {
-    dialog.close();
-});
-
-// SHOW IMAGE PREVIEW IN FORM
-const imageLinkInput = document.getElementById('image-link');
-const imagePreview = document.getElementById('image-preview');
-
-if (imageLinkInput) {
-    imageLinkInput.addEventListener('input', () => {
-        const imageLink = imageLinkInput.value.trim();
-
-        if (validateLink(imageLink)) {
-            imagePreview.innerHTML = `<img src="${imageLink}" alt="Preview van gekozen afbeelding">`;
-        } else {
-            imagePreview.innerHTML = '';
+    function displayError(errorMessage, inputElement) {
+        if (!inputElement.classList.contains('error')) {
+            inputElement.insertAdjacentHTML('afterend', `<span class="error">${errorMessage}</span>`);
+            inputElement.classList.add('error');
+            document.querySelector('input:invalid, textarea:invalid, .error input').focus();
         }
+    }
+
+    function removeErrorMessage(inputElement) {
+        if (inputElement.classList.contains('error')) {
+            const errorElement = inputElement.nextElementSibling;
+            errorElement.parentNode.removeChild(errorElement);
+            inputElement.classList.remove('error');
+        }
+    }
+
+    function validateLink(link) {
+        try {
+            const url = new URL(link);
+            return url.protocol === 'http:' || url.protocol === 'https:';
+        } catch (error) {
+            return false;
+        }
+    }
+
+    // DIALOG
+    const dialogBtn = document.querySelector('#dialog-btn');
+    const dialog = document.querySelector('#dialog');
+    const dialogCloseBtn = document.querySelector('#dialog-close');
+
+    dialogBtn.addEventListener('click', () => {
+        dialog.showModal();
     });
+
+    dialogCloseBtn.addEventListener('click', () => {
+        dialog.close();
+    });
+
+    // SHOW IMAGE PREVIEW IN FORM
+    const imageLinkInput = document.getElementById('image-link');
+    const imagePreview = document.getElementById('image-preview');
+
+    if (imageLinkInput) {
+        imageLinkInput.addEventListener('input', () => {
+            const imageLink = imageLinkInput.value.trim();
+
+            if (validateLink(imageLink)) {
+                imagePreview.innerHTML = `<img src="${imageLink}" alt="Preview van gekozen afbeelding">`;
+            } else {
+                imagePreview.innerHTML = '';
+            }
+        });
+    }
 }
